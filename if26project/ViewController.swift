812d818276d61a387685f83dbdacfd0dc4bc6a22
CACHARD MARC-ANTOINE <cachardm@mac04-m102.utt.fr>
2019-01-10 13:56:30 +0100

    Initial Commit

---
+++ if26project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  if26project
+//
+//  Created by CACHARD MARC-ANTOINE on 10/01/2019.
+//  Copyright © 2019 CACHARD MARC-ANTOINE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
