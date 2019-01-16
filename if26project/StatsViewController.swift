d5b8e8282406345c978a10e1bd3311f19d31efa7
CACHARD MARC-ANTOINE <cachardm@mac04-m102.utt.fr>
2019-01-16 15:43:06 +0100

    end project

--- if26project/StatsViewController.swift
+++ if26project/StatsViewController.swift
@@ -25,10 +25,10 @@ class StatsViewController: UIViewController {
         self.lblEnseignants.text = "Il y a "+String(nms[0])+" enseignants dans le département dont "+String(nms[3])+" professeurs, "+String(nms[2])+" maitres de conférence, "+String(nms[1])+" contractuels"
         
         nms = db.countModules()
-        self.lblModules.text = "Il y a "+String(nms[0])+"modules dans le département dont "+String(nms[1])+" CS et "+String(nms[2])+" TM"
+        self.lblModules.text = "Il y a "+String(nms[0])+" modules dans le département dont "+String(nms[1])+" CS et "+String(nms[2])+" TM"
         
         nms = db.countEtudiants()
-        self.lblEtudiants.text = "Il y a "+String(nms[0])+"etudiants dans le département dont "+String(nms[1])+" en MSI, "+String(nms[2])+" en MCS et "+String(nms[2])+" en MPL"
+        self.lblEtudiants.text = "Il y a "+String(nms[0])+" etudiants dans le département dont "+String(nms[1])+" en MSI, "+String(nms[2])+" en MCS et "+String(nms[2])+" en MPL"
     }
 }
 
